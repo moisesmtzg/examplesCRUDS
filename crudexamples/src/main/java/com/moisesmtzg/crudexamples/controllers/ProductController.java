b5a2bdd757e33cfa283b638f9f153acf9b787fb0
Moises <moises@MacBook-Pro-de-Moises.local>
2026-06-11 15:29:56 -0600

    part 2

--- crudexamples/src/main/java/com/moisesmtzg/crudexamples/controllers/ProductController.java
+++ crudexamples/src/main/java/com/moisesmtzg/crudexamples/controllers/ProductController.java
@@ -2,14 +2,19 @@ package com.moisesmtzg.crudexamples.controllers;
 
 import com.moisesmtzg.crudexamples.entities.Product;
 import com.moisesmtzg.crudexamples.services.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Product controller that handles http requests
+ */
 @RestController
 @RequestMapping("/api/products")
+@Slf4j
 public class ProductController {
 
     private final ProductService productService;
@@ -21,35 +26,37 @@ public class ProductController {
 
     @GetMapping
     public List<Product> getAll(){
-        System.out.println("trying to fetch all products");
+        log.info("Fetching all products");
         return productService.getProducts();
     }
 
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable long id){
-        System.out.println("trying to fetch product with id:" + id);
-        return productService.findProduct(id).orElseThrow();
+        log.info("Getting product by id:{}", id);
+        return productService.findProduct(id);
     }
 
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable long id){
-        System.out.println("trying to delete product with id:" + id);
+        log.info("Delete product with id:{}",id);
         productService.deleteProduct(id);
     }
 
     @PostMapping
     public Product saveProduct(@RequestBody Product newProduct){
-        System.out.println("trying to add new product");
+        log.info("Saving product: {}", newProduct);
         return productService.saveProduct(newProduct);
     }
 
     @PutMapping("/{id}")
     public Product updateProduct(@PathVariable long id, @RequestBody Product product){
+        log.info("Updating product with id:{}", id);
         return productService.updateProduct(id, product);
     }
 
     @PatchMapping("{id}")
     public Product partialUpdateProduct(@PathVariable long id, @RequestBody Map<String, Object> valuesToUpdate){
+        log.info("Patching product with id:{}", id);
         return productService.patchProduct(id, valuesToUpdate);
     }
 }
